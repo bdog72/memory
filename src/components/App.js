00a114579aa13b574f2bb5094cfef1366c5c3d14
Brian Bycynski <high5bri@hotmail.com>
2016-12-08 18:32:23 -0500

    memory

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,57 @@
 import React, { Component } from 'react'
 
+import images from './images'
+
 class App extends Component {
 
   render () {
     return <div>
-      <h1>Hello, World!</h1>
+      {/* <h1>Memory</h1> */}
+      {/* <img src={images[0]} height='150' width='150' />
+      <img src={images[1]} height='150' width='150' />
+      <img src={images[2]} height='150' width='150' />
+      <img src={images[3]} height='150' width='150' />
+      <img src={images[4]} height='150' width='150' />
+      <img src={images[5]} height='150' width='150' />
+      <img src={images[6]} height='150' width='150' />
+      <img src={images[7]} height='150' width='150' />
+      <img src={images[8]} height='150' width='150' /> */}
+
+      <table>
+
+        <tr>
+          <td><img src={images[0]} height='125' width='125' /></td>
+          <td><img src={images[1]} height='125' width='125' /></td>
+          <td><img src={images[2]} height='125' width='125' /></td>
+          <td><img src={images[3]} height='125' width='125' /></td>
+          <td><img src={images[4]} height='125' width='125' /></td>
+          <td><img src={images[5]} height='125' width='125' /></td>
+        </tr>
+        <tr>
+          <td><img src={images[6]} height='125' width='125' /></td>
+          <td><img src={images[7]} height='125' width='125' /></td>
+          <td><img src={images[8]} height='125' width='125' /></td>
+          <td><img src={images[9]} height='125' width='125' /></td>
+          <td><img src={images[10]} height='125' width='125' /></td>
+          <td><img src={images[11]} height='125' width='125' /></td>
+        </tr>
+        <tr>
+          <td><img src={images[12]} height='125' width='125' /></td>
+          <td><img src={images[13]} height='125' width='125' /></td>
+          <td><img src={images[14]} height='125' width='125' /></td>
+          <td><img src={images[15]} height='125' width='125' /></td>
+          <td><img src={images[16]} height='125' width='125' /></td>
+          <td><img src={images[17]} height='125' width='125' /></td>
+        </tr>
+        <tr>
+          <td><img src={images[18]} height='125' width='125' /></td>
+          <td><img src={images[19]} height='125' width='125' /></td>
+          <td><img src={images[20]} height='125' width='125' /></td>
+          <td><img src={images[21]} height='125' width='125' /></td>
+          <td><img src={images[22]} height='125' width='125' /></td>
+          <td><img src={images[23]} height='125' width='125' /></td>
+        </tr>
+      </table>
     </div>
   }
 }
---
+++ src/components/images.js
@@ -0,0 +1,28 @@
+const images = [
+  'http://i.cdn.turner.com/v5cache/CARTOON/site/Images/i58/lt_wp_bugs3_800x600.jpg',
+  'http://i.cdn.turner.com/v5cache/CARTOON/site/Images/i58/lt_wp_bugs1_800x600.jpg',
+  'http://i.cdn.turner.com/v5cache/CARTOON/site/Images/i30/lt_wp_daffy_800x600.jpg',
+  'http://i.cdn.turner.com/v5cache/CARTOON/site/Images/i30/lt_wp_daffy2_800x600.jpg',
+  'http://i.cdn.turner.com/v5cache/CARTOON/site/Images/i30/lt_wp_speedy_800x600.jpg',
+  'http://i.cdn.turner.com/v5cache/CARTOON/site/Images/i30/lt_wp_speedy1_800x600.jpg',
+  'http://i.cdn.turner.com/v5cache/CARTOON/site/Images/i30/lt_wp_porky_800x600.jpg',
+  'http://i.cdn.turner.com/v5cache/CARTOON/site/Images/i30/lt_wp_porky1_800x600.jpg',
+  'http://i.cdn.turner.com/v5cache/CARTOON/site/Images/i58/lt_wp_yosemitesam2_800x600.jpg',
+  'http://i.cdn.turner.com/v5cache/CARTOON/site/Images/i58/lt_wp_generics2_800x600.jpg',
+  'http://i.cdn.turner.com/v5cache/CARTOON/site/Images/i30/lt_wp_foghorn_800x600.jpg',
+  'http://i.cdn.turner.com/v5cache/CARTOON/site/Images/i30/lt_wp_grantweetsylv_800x600.jpg',
+  'http://i.cdn.turner.com/v5cache/CARTOON/site/Images/i58/lt_wp_bugs3_800x600.jpg',
+  'http://i.cdn.turner.com/v5cache/CARTOON/site/Images/i58/lt_wp_bugs1_800x600.jpg',
+  'http://i.cdn.turner.com/v5cache/CARTOON/site/Images/i30/lt_wp_daffy_800x600.jpg',
+  'http://i.cdn.turner.com/v5cache/CARTOON/site/Images/i30/lt_wp_daffy2_800x600.jpg',
+  'http://i.cdn.turner.com/v5cache/CARTOON/site/Images/i30/lt_wp_speedy_800x600.jpg',
+  'http://i.cdn.turner.com/v5cache/CARTOON/site/Images/i30/lt_wp_speedy1_800x600.jpg',
+  'http://i.cdn.turner.com/v5cache/CARTOON/site/Images/i30/lt_wp_porky_800x600.jpg',
+  'http://i.cdn.turner.com/v5cache/CARTOON/site/Images/i30/lt_wp_porky1_800x600.jpg',
+  'http://i.cdn.turner.com/v5cache/CARTOON/site/Images/i58/lt_wp_yosemitesam2_800x600.jpg',
+  'http://i.cdn.turner.com/v5cache/CARTOON/site/Images/i58/lt_wp_generics2_800x600.jpg',
+  'http://i.cdn.turner.com/v5cache/CARTOON/site/Images/i30/lt_wp_foghorn_800x600.jpg',
+  'http://i.cdn.turner.com/v5cache/CARTOON/site/Images/i30/lt_wp_grantweetsylv_800x600.jpg'
+]
+
+export default images
